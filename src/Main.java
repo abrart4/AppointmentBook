2f1044c895c3b1dc1c523062b2e1fbfa87f7e35a
AbrarTaseen <abrart4@nycstudents.net>
2025-10-11 16:32:07 -0400

    Add AP test cases

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-        boolean[][] schedule = new boolean[8][60];
-        AppointmentBook a = new AppointmentBook(schedule);
-        for (int i = 10; i < 15; i ++) {
-            schedule[1][i] = true;
+        System.out.println("Test case 1 (findFreeBlock)");
+        boolean[][] test1$schedule = new boolean[8][60];
+        AppointmentBook test1$book = new AppointmentBook(test1$schedule);
+        for (int i = 10; i <= 14; i ++) {
+            test1$schedule[1][i] = true;
         }
-
-        a.printPeriod(2);
-
-        for (int i = 25; i < 30; i ++) schedule[1][i] = true;
-        for (int i = 0; i < 15; i ++) schedule[2][i] = true;
-        for (int i = 41; i < 60; i ++) schedule[2][i] = true;
-        for (int i = 5; i < 30; i ++) schedule[3][i] = true;
-        for (int i = 44; i < 60; i ++) schedule[3][i] = true;
-
-        AppointmentBook b = new AppointmentBook(schedule);
-        int period = 2;
-        while (period < 5) {
-            System.out.println("Period: " + period);
-            b.printPeriod(period);
-            period ++;
+        for (int i = 30; i <= 44; i ++) {
+            test1$schedule[1][i] = true;
         }
+        for (int i = 50; i <= 59; i ++) {
+            test1$schedule[1][i] = true;
+        }
+        System.out.println("Printing period 2");
+        test1$book.printPeriod(2);
+        System.out.println("book.findFreeBlock(2, 15): " + test1$book.findFreeBlock(2, 15));
+        System.out.println("book.findFreeBlock(2, 9): " + test1$book.findFreeBlock(2, 9));
+        System.out.println("book.findFreeBlock(2, 20): " + test1$book.findFreeBlock(2, 20));
 
-        b.printPeriod(4);
-        System.out.println(b.makeAppointment(2, 4, 22));
-        b.printPeriod(4);
+        System.out.println("Test case 2 (makeAppointment)");
+        boolean[][] test2$schedule = new boolean[8][60];
+        AppointmentBook test2$book = new AppointmentBook(test2$schedule);
+        for (int i = 25; i <= 29; i ++) {
+            test2$schedule[1][i] = true;
+        }
+        for (int i = 0; i <= 14; i ++) {
+            test2$schedule[2][i] = true;
+        }
+        for (int i = 41; i <= 59; i ++) {
+            test2$schedule[2][i] = true;
+        }
+        for (int i = 5; i <= 29; i ++) {
+            test2$schedule[3][i] = true;
+        }
+        for (int i = 44; i <= 59; i ++) {
+            test2$schedule[3][i] = true;
+        }
+        System.out.println("Printing period 2");
+        test2$book.printPeriod(2);
+        System.out.println("Printing period 3");
+        test2$book.printPeriod(3);
+        System.out.println("Printing period 4");
+        test2$book.printPeriod(4);
+        System.out.println("book.makeAppointment(2, 4, 22): " + test2$book.makeAppointment(2, 4, 22));
+        System.out.println("book.makeAppointment(3, 4, 3): " + test2$book.makeAppointment(3, 4, 3));
+        System.out.println("book.makeAppointment(2, 4, 30): " + test2$book.makeAppointment(2, 4, 30));
+        System.out.println("Printing period 2");
+        test2$book.printPeriod(2);
+        System.out.println("Printing period 3");
+        test2$book.printPeriod(3);
+        System.out.println("Printing period 4");
+        test2$book.printPeriod(4);
     }
 }
